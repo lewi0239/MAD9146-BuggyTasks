9a61839bdfaddca5ed7f351022bb07620ba539e1
Brodie Lewis <lewi0239@algonquinlive.com>
2025-07-27 19:57:06 -0400

    fixed AddNewTaskPage name misspelling

--- BuggyTasks/Views/NewTaskPage.xaml.cs
+++ BuggyTasks/Views/NewTaskPage.xaml.cs
@@ -1,8 +1,4 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using BuggyTasks.ViewModels;  
 
 namespace BuggyTasks.Views;
 
@@ -11,5 +7,7 @@ public partial class NewTaskPage : ContentPage
     public NewTaskPage()
     {
         InitializeComponent();
+
+        BindingContext = new NewTaskViewModel();
     }
 }
